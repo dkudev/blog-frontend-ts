57cd8b36e1e4fda492c7eff97f21f7a188e26282
Dimitar Kudev <dimitar.kudev@lecturio.de>
2021-11-17 11:23:22 +0200

    Auth Provider...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const auth = useAuth0();
+  const { loginWithRedirect, logout } = auth;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => loginWithRedirect()}>Log In</button>;
+        <button onClick={() => logout()}>Log OUT</button>;
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/context/Auth.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Auth0Provider as Auth } from "@auth0/auth0-react";
+
+const Auth0ClientOptions: {
+  domain: string;
+  clientId: string;
+  redirectUri: string;
+} = {
+  domain: `${process.env.REACT_APP_AUTH0_DOMAIN}`,
+  clientId: `${process.env.REACT_APP_AUTH0_CLIENT_ID}`,
+  redirectUri: window.location.origin,
+};
+
+export function AuthProvider({ children }: any) {
+  return <Auth {...Auth0ClientOptions}>{children}</Auth>;
+}
